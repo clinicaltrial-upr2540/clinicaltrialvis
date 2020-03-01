6fc96c1f857ac74ad8e42af2f6b1d032661f9a1b
Gabriel Sanna <gabrielsanna@gmail.com>
2020-03-01 16:14:13 -0500

    Adding more app structure

--- app/app.py
+++ app/app.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template, session, redirect, request
 
 app = Flask(__name__)
 
+############################################
+# Startup tasks go here (load/check data)
+############################################
+
+
+
+############################################
+# Routes to web pages go here
+############################################
+
+# Route to homepage
 @app.route("/")
 def index():
 	return render_template('home.html')
