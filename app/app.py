52dbb58c63c0b4da53ef5fcc60e37e7c8d09cdc4
q0w9e8r7t6y5 <syeety@me.com>
2020-03-15 15:28:06 -0400

    added server information

--- app/app.py
+++ app/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, session, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+application = Flask(__name__)
 
 app = Flask(__name__)
+app.config['TESTING'] = True
 
 ############################################
 # Startup tasks go here (load/check data)
 ############################################
 
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://postgres:y9fBsh5xEeYvkUkCQ5q3@drugdata.cgi8bzi5jc1o.us-east-1.rds.amazonaws.com/drugdata'
+db = SQLAlchemy(application)
 
 ############################################
 # Routes to web pages go here
@@ -20,3 +24,6 @@ def index():
 @app.route("/test")
 def test():
 	return render_template('test.html')
+
+if __name__ == "__main__":
+	app.run(debug=True)
