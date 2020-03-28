41900395a1ddae9f2535459eb90e9e1b1b0445b2
gabrielsanna-toast <gabriel.sanna@toasttab.com>
2020-03-28 11:12:20 -0400

    Added API endpoint to get list of tables

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import json
+import sqlalchemy
+
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 
 app = Flask(__name__)
 app.config['TESTING'] = True
@@ -10,9 +14,14 @@ app.config['TESTING'] = True
 # Startup tasks go here (load/check data)
 ############################################
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:y9fBsh5xEeYvkUkCQ5q3@drugdata.cgi8bzi5jc1o.us-east-1.rds.amazonaws.com/drugdata'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# Connect to the database
+# Set the DB URL and schema to use
+# URL format: postgresql://<username>:<password>@<hostname>:<port>/<database>
+DATABASE_URL = "postgresql://postgres:y9fBsh5xEeYvkUkCQ5q3@drugdata.cgi8bzi5jc1o.us-east-1.rds.amazonaws.com:5432/drugdata"
+
+# Set up and establish connection
+engine = sqlalchemy.create_engine(DATABASE_URL)
+db = scoped_session(sessionmaker(bind=engine))
 
 
 ############################################
@@ -34,6 +43,26 @@ def render_test():
 @app.route("/visualizations")
 def render_visualizations():
     return render_template('visualizations.html', page_title="Visualizations")
+
+
+############################################
+# Routes to API endpoints go here
+############################################
+
+@app.route("/data/tables")
+def data_tables():
+    table_list = []
+
+    # Query tables in the 'curated' schema and serialize
+    result = db.execute(f"SELECT * FROM information_schema.tables WHERE table_schema = \'curated\';")
+    result = [dict(row) for row in result]
+
+    # Loop through and pull out table names
+    for table in result:
+        table_list.append(table["table_name"])
+
+    # Return table name as JSON object
+    return(json.dumps(table_list, indent=4, separators=(',', ': ')))
 
 
 if __name__ == "__main__":
