cf3195b9ffe7dab617f0f1fc423dbe7e935e248b
jddowey <jdowey@g.harvard.edu>
2020-03-30 00:03:06 +0200

    first viz correction, dotChartSetup.js replaced with chartSetup.js

--- app/static/js-visualizations/dotChartSetup.js
+++
@@ -1,9 +0,0 @@
-var dot_chart_options = {
-		    dot_radius : 5,
-		    no_of_circles_in_a_row: 40,
-            dot_padding_left : 5,
-            dot_padding_right : 5,
-            dot_padding_top : 5,
-            dot_padding_bottom : 5
-		}
-DotMatrixChart(dataset,dot_chart_options);
