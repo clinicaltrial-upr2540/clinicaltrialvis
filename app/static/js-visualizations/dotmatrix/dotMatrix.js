364167d30726c35d3ff044b8712abb6b7250c7d3
jddowey <jdowey@g.harvard.edu>
2020-04-22 19:10:03 +0200

    two remaining visulaizations for code review, dotmatrix, radar

--- app/static/js-visualizations/dotmatrix/dotMatrix.js
+++ app/static/js-visualizations/dotmatrix/dotMatrix.js
@@ -1,9 +1,11 @@
 
-//from https://github.com/arpitnarechania/d3-dotmatrix/blob/master/dist/DotMatrix.js
-//rewritten from d3v3 to d3v4 by me
+//inspired by d3v3 from https://github.com/arpitnarechania/d3-dotmatrix/blob/master/dist/DotMatrix.js
+//rewritten from d3v3 to d3v4 and adjusted to the company-> disease_class -> drugs dataset by Jelena Dowey
 
+//create a dotMatrix chart
 function dotMatrixChart(dataset,options){
 
+    //settings for dot chart
     var dotRadius = options.dot_radius;
     var noOfCirclesInARow = options.no_of_circles_in_a_row;
     var dotPaddingLeft = options.dot_padding_left;
@@ -11,6 +13,7 @@ function dotMatrixChart(dataset,options){
     var dotPaddingTop = options.dot_padding_top;
     var dotPaddingBottom = options.dot_padding_bottom;
 
+    //error handling for the lack of settings
     if(isNaN(dotRadius)){
         throw new Error("dot_radius must be a Number");
     }
@@ -31,6 +34,7 @@ function dotMatrixChart(dataset,options){
     }
 
 
+    // create data collections for the drug class (categories) and companies 
     var flags = [], uniqueCategories = [], uniqueCompanies=[], l = dataset.length, i;
     for( i=0; i<l; i++) {
         if( flags[dataset[i].disease_class]) continue;
@@ -43,7 +47,7 @@ function dotMatrixChart(dataset,options){
         flags[dataset[i].company] = true;
         uniqueCompanies.push(dataset[i].company);
     }
-
+    //create a data collection for a number of drugs approved by each company
     var sumOfEveryCompany = {};
     for(var i=0;i<dataset.length;i++){
         if(sumOfEveryCompany[dataset[i]['company']] == null){
@@ -52,6 +56,7 @@ function dotMatrixChart(dataset,options){
         sumOfEveryCompany[dataset[i]['company']] += dataset[i]['counts'];
     }
 
+    //create a variable that defines a max number of lines per company in visualization
     var maxNoOfLinesInCompany = 0;
     for(var company in sumOfEveryCompany){
         if(sumOfEveryCompany[company]/noOfCirclesInARow > maxNoOfLinesInCompany){
@@ -59,22 +64,24 @@ function dotMatrixChart(dataset,options){
         }
     }
 
+    //create a variable that defines a total number of lines in visualization
     var numberOfLines = maxNoOfLinesInCompany * uniqueCompanies.length;
 
+    //create a company and disease class scale
     var companyScale = d3.scalePoint().domain(uniqueCompanies).range([0, uniqueCompanies.length-1]);
     var diseaseClassScale = d3.scalePoint().domain(uniqueCategories).range([0, uniqueCategories.length]);
 
-    //var color = d3.scaleOrdinal(d3.schemeDark2);
-    
+    //create a color scale
+    //d3 scales can be viewed here https://github.com/d3/d3-scale-chromatic
+    //schemeDark2 has 8 colors
     var color = d3.scaleOrdinal(d3.schemeDark2);
 
-    // Set the dimensions of the canvas / graph
+    // set dimensions and margins of the visualization
     var	margin = {top: dotRadius*10, right: dotRadius*15, bottom: dotRadius*10, left: dotRadius*15};
-
     height = numberOfLines * (dotRadius*2 + dotPaddingBottom + dotPaddingTop);
     width = (dotRadius*2 + dotPaddingLeft + dotPaddingRight) * noOfCirclesInARow;
 
-    // Set the ranges
+    // set x-axis and y-axis - scale, range, axis, ticks, domain
     var	xScale = d3.scaleLinear().range([margin.left, width]);
     var	yScale = d3.scaleLinear().range([height, margin.bottom]);
 
@@ -95,14 +102,14 @@ function dotMatrixChart(dataset,options){
     xScale.domain([0,noOfCirclesInARow]);
     yScale.domain([0,d3.max(dataset,function(d){return companyScale(d.company)+1;})]);
 
-    //Create SVG element
+    //create and append the svg element to the dotMatrixChart element of the page
     var svg = d3.select("#dotMatrixChart")
                 .append("svg")
                 .attr("width", width + margin.left + margin.right + width/2)
                 .attr("height", height + margin.top + margin.bottom)
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    //Create Y axis
+    //populate y axis
     svg.append("g")
         .attr("transform", "translate(" + (margin.left - (dotRadius*2)) + ",0)")
         .attr("class", "y axis")
@@ -115,7 +122,7 @@ function dotMatrixChart(dataset,options){
         .attr("transform", "rotate(-90)")
         .style("text-anchor", "start");
 
-    //Create Y axis
+    //populate x axis
         svg
         .append("line")
         .attr("x1",width)
@@ -125,9 +132,11 @@ function dotMatrixChart(dataset,options){
         .style("stroke","black")
         .style("stroke-width",1)
 
+    //create an array of dots and their positionsnin the lines
     var globalLineNoForCompany = {};
     var globalLineSizeForCompany = {};
     var globalDotXPosition = {};
+    
     function generate_array(d){
 
         if(globalLineSizeForCompany[d.company] == null){
@@ -156,7 +165,7 @@ function dotMatrixChart(dataset,options){
         return arr;
     }
   
-
+    //create company element and append it to the DOM
     var companies = svg
        .selectAll("g.company")
        .data(dataset)
@@ -164,9 +173,11 @@ function dotMatrixChart(dataset,options){
         .append('g')
         .attr("class", "company");
 
+    //create circle elements (array fo circles) and append them to the DOM
     var circleArray = companies.selectAll("g.circleArray")
     .data(function(d) {return generate_array(d);});
 
+    //color circle elements
     circleArray.enter()
     .append('g')
     .attr("class", "circleArray")
@@ -176,7 +187,7 @@ function dotMatrixChart(dataset,options){
     .attr("cx", function(d,i) {return xScale(d.x); })
     .attr("cy", function(d,i) { return yScale(d.y); });
 
-    // add legend
+    // add  and style legend, color by colorscheme according to the disease class
     var legend = svg
     .selectAll(".legend")
     .data(uniqueCategories)
@@ -202,11 +213,12 @@ function dotMatrixChart(dataset,options){
       .style("font-size", dotRadius*3 + "px")
       .text(function(d){return d});
 
-
+    //create tooltip
     var tooltip = d3.select("body")
     .append('div')
     .attr('class', 'tooltip');
 
+    //populate tooltip with a name of the company, disease class and a number of drugs within the disease class
     tooltip.append('div')
     .attr('class', 'company');
     tooltip.append('div')
@@ -214,6 +226,7 @@ function dotMatrixChart(dataset,options){
     tooltip.append('div')
     .attr('class', 'number_of_drugs');
 
+    //add mouse over, mouse move and mouse out behavior that shows a tooltip on hovering over the circles
     svg.selectAll(".circleArray > circle")
     .on('mouseover', function(d,i) {
 
@@ -233,4 +246,4 @@ function dotMatrixChart(dataset,options){
         tooltip.style('display', 'none');
         tooltip.style('opacity',0);
     });
-}
+} //end dotMatrixChart function
