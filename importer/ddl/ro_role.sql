b7e1b1a0590c3c599598ec6c406df155acc6d13b
gabrielsanna-toast <gabriel.sanna@toasttab.com>
2020-05-13 19:52:21 -0400

    SQL script to create application read-only role

---
+++ importer/ddl/ro_role.sql
@@ -0,0 +1,15 @@
+
+BEGIN;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'ro_role') THEN
+        CREATE ROLE ro_role;
+    END IF;
+END
+$$;
+GRANT usage ON SCHEMA curated TO ro_role;
+GRANT SELECT ON ALL TABLES IN SCHEMA curated TO ro_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA curated GRANT SELECT ON TABLES TO ro_role;
+
+COMMIT;
